5a70095f5032e23f54f9450b72f6db5fa8965790
joseph <joseph@novanus.group>
2020-06-04 06:43:40 +0800

    Updates for message handlers and senders.

--- src/handler/postback-handler.service.ts
+++ src/handler/postback-handler.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { IHandler } from "./handler.service";
+import { IMessage } from "src/model/message.model";
 
 @Injectable()
 export class PostbackHandlerService implements IHandler {
 
     constructor() {}
     
-    doHandle(sender_id: string, message: any) {
+    doHandle(sender_id: string, message: any): IMessage {
         console.error('@@@ [POSTBACK] sender: ', sender_id, ' | message: ', message);
+        return null;
     }
 }
